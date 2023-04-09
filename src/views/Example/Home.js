deb3db398d5638a9fb693ca016084ef47e32b59e
Quan Nt <quannt210399@gmail.com>
2023-04-09 10:21:32 +0700

    #22 Redirect with React Routers and HOC

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export const withRouter = (WrappedComponent) => (props) => {
+    return <WrappedComponent {...props} navigate={useNavigate()} />;
+};
 
 class Home extends React.Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.navigate("/todo");
+        }, 3000);
+    }
+    //HOC: higher order component
+
     render() {
         return <div>Hello world from Homepage with Eric & Hoi Dan IT</div>;
     }
 }
 
-export default Home;
+export default withRouter(Home);
