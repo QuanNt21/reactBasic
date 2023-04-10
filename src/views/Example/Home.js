67a290c0d3931407bf5231c8612843d336c119f7
Quan Nt <quannt210399@gmail.com>
2023-04-10 22:58:31 +0700

    #25 Setup Redux

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "../HOC/withRouter";
+import logo from "../../assets/images/logoChannel.png";
 
 class Home extends React.Component {
     componentDidMount() {
@@ -10,7 +11,21 @@ class Home extends React.Component {
     //HOC: higher order component
 
     render() {
-        return <div>Hello world from Homepage with Eric & Hoi Dan IT</div>;
+        return (
+            <>
+                <div>Hello world from Homepage with Eric & Hoi Dan IT</div>
+                <div>
+                    <img
+                        src={logo}
+                        style={{
+                            width: "200px",
+                            height: "200px",
+                            marginTop: "20px",
+                        }}
+                    />
+                </div>
+            </>
+        );
     }
 }
 
